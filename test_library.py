8290d4417eab1dd5c80600a131839185b78bcc42
ssanikajog <sanikajog1296@gmail.com>
2026-01-08 14:38:19 +0530

    save

---
+++ test_library.py
@@ -0,0 +1,11 @@
+from library import library_details
+
+def test_library_details(ID, title, Author, Year):
+    expected_output = (
+        "Book ID: 1001\n",
+        "Book Title: Train Dreams\n",
+        "Author Name: Dennis J\n",
+        "Year Of Publication: 2015\n"
+    )
+
+    assert (library_details("1001", "Train Dreams", "Dennis J", "2015")) == expected_output
