06319d52f776319d351011122e17cc44923b345d
ssanikajog <sanikajog1296@gmail.com>
2026-01-08 15:15:36 +0530

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -1,11 +1,11 @@
 from library import library_details
 
-def test_library_details(id, title, Author, Year):
+def test_library_details():
     expected_output = (
-        "Book ID: 1001\n",
-        "Book Title: Train Dreams\n",
-        "Author Name: Dennis J\n",
-        "Year Of Publication: 2015\n"
+        f"Book ID: 1001\n"
+        f"Book Title: Train Dreams\n"
+        f"Author Name: Dennis J\n"
+        f"Year Of Publication: 2015\n"
     )
 
-    assert (library_details("1001", "Train Dreams", "Dennis J", "2015")) == expected_output
+    assert library_details("1001", "Train Dreams", "Dennis J", "2015" == expected_output)
